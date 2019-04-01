db8f4163907c468c357e7c3d51e56e4d47bf3d72
Brandon Sandoval <colima_97@hotmail.com>
2019-04-01 16:08:01 -0600

    Creé en la clase main la estructura switch case. En la clase main creé un método llamado menu_principal(), en el cual aparecen las opciones "Op1","Op2","Op3" y "Op4", las cuales deberán ser cambiadas por las opciones del menú principal. Hablando del método anterior, este resgresa un entero que es recogido en la clase main por el switch case.

--- src/analizador_lexico/Analizador_L.java
+++ src/analizador_lexico/Analizador_L.java
@@ -1,9 +1,38 @@
 package analizador_lexico;
+import javax.swing.JOptionPane;
 
-public class Analizador_L_main {
-
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
+public class Analizador_L{    
+    
+    public static void main(String[] args) {                       
+        int valor = menu_principal();
+        switch(valor){
+            case 0:
+                //Your code here! @1
+                break;
+            
+            case 1:
+                //Your code here! @2
+                break;
+            
+            case 2:
+                //Your code here! @3
+                break;
+                
+            case 3:
+                //Your code here! @4
+                break;                
+        }
+                
+        
     }
     
+    public static int menu_principal(){
+        MyIcon icon = new MyIcon();
+        String[] options = {
+          "Op1","Op2","Op3","Op4"  
+        };
+        int seleccion = JOptionPane.showOptionDialog(null, "Seleccione una opción", "Menú principal", JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE, icon,options,options[0]);
+        return seleccion;
+    }
+        
 }
