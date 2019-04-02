c691b7d9a9a03c34cfb2fd22ef53c3d1ce24d7c7
Brandon Sandoval <colima_97@hotmail.com>
2019-04-01 18:02:47 -0600

    Agregué varios métodos a la clase principal, tales como: menu_insercion() - Aquí se lee la string menu_salida() - Sólo es un mensaje de despedida error_message() - Muestra que ocurrió un error

--- src/analizador_lexico/Analizador_L.java
+++ src/analizador_lexico/Analizador_L.java
@@ -3,36 +3,62 @@ import javax.swing.JOptionPane;
 
 public class Analizador_L{    
     
+   static  MyIcon icon;
     public static void main(String[] args) {                       
-        int valor = menu_principal();
-        switch(valor){
-            case 0:
-                //Your code here! @1
-                break;
-            
-            case 1:
-                //Your code here! @2
-                break;
-            
-            case 2:
-                //Your code here! @3
-                break;
-                
-            case 3:
-                //Your code here! @4
-                break;                
+
+        while(true){
+            int valor = menu_principal();
+            switch(valor){
+                case 0:
+                    String respuesta = menu_insercion();
+
+                    if(respuesta == null || respuesta.isEmpty()){
+                        error_message();                        
+                    }else{
+                        //
+                    }
+                    break;
+
+                case 1:
+                    //Your code here! @2
+                    break;
+
+                case 2:
+                    //Your code here! @3
+                    break;
+
+                case 3:
+                    menu_salida();
+                    return;                                    
+            }
         }
                 
         
     }
     
-    public static int menu_principal(){
-        MyIcon icon = new MyIcon();
+    public static int menu_principal(){   
+        icon = new MyIcon();
         String[] options = {
-          "Op1","Op2","Op3","Op4"  
+          "Escribir cadena","Mostrar anteriores","Ayuda","Salir"  
         };
-        int seleccion = JOptionPane.showOptionDialog(null, "Seleccione una opción", "Menú principal", JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE, icon,options,options[0]);
+        int seleccion = JOptionPane.showOptionDialog(null, "Seleccione una opción", "Menú principal", 0,0, icon,options,options[0]);
         return seleccion;
+    }
+    
+    public static String menu_insercion(){
+        icon = new MyIcon();
+        String seleccion = JOptionPane.showInputDialog(null, "Escriba una cadena", "Escribir cadena", -1);
+        return seleccion;
+    }
+    
+    public static void menu_salida(){
+        icon = new MyIcon();
+        JOptionPane.showMessageDialog(null, "Hasta luego", "Salir", 0, icon);
+    }
+    
+    public static void error_message(){
+        icon = new MyIcon();
+        JOptionPane.showMessageDialog(null, "Ups, algo salió mal", "Error", JOptionPane.ERROR_MESSAGE);
     }
         
 }
