8196c7213e7b7554a722bf6943c2ce9f50689c14
Brandon Sandoval <colima_97@hotmail.com>
2019-04-04 22:25:41 -0600

    Agregué título. Agregué comportamiento de cierre. Agregué orientación al centro de la pantalla.

--- src/analizador_lexico/Analizador_L.java
+++ src/analizador_lexico/Analizador_L.java
@@ -3,10 +3,11 @@ import javax.swing.JOptionPane;
 
 public class Analizador_L{    
     
-   static  MyIcon icon;
+    static  MyIcon icon;
     public static void main(String[] args) {                       
         Ventana v1 = new Ventana();
-        v1.setVisible(true);
+        v1.setVisible(true);       
+        
     }
     
     public static void error_message(){
--- src/analizador_lexico/Ventana.java
+++ src/analizador_lexico/Ventana.java
@@ -1,11 +1,18 @@
 package analizador_lexico;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Ventana extends JFrame{
     
     public Ventana(){
-        this.setSize(600, 600);
-        
+        setSize(600,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Analizador Léxico");
+        setLocationRelativeTo(null);
+    }
+    
+    private void iniciarComponentes(){
+        JPanel panel = new JPanel();
     }
     
 }
