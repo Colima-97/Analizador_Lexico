98cea3a8cdeefa3b89799b9b0b1db5f2b85914b4
Brandon Sandoval <colima_97@hotmail.com>
2019-04-04 22:01:40 -0600

    Creé una ventana JFrame de 600*600px y creé la instancia en el menú Main

--- src/analizador_lexico/Analizador_L.java
+++ src/analizador_lexico/Analizador_L.java
@@ -5,55 +5,8 @@ public class Analizador_L{
     
    static  MyIcon icon;
     public static void main(String[] args) {                       
-
-        while(true){
-            int valor = menu_principal();
-            switch(valor){
-                case 0:
-                    String respuesta = menu_insercion();
-
-                    if(respuesta == null || respuesta.isEmpty()){
-                        error_message();                        
-                    }else{
-                        //
-                    }
-                    break;
-
-                case 1:
-                    //Your code here! @2
-                    break;
-
-                case 2:
-                    //Your code here! @3
-                    break;
-
-                case 3:
-                    menu_salida();
-                    return;                                    
-            }
-        }
-                
-        
-    }
-    
-    public static int menu_principal(){   
-        icon = new MyIcon();
-        String[] options = {
-          "Escribir cadena","Mostrar anteriores","Ayuda","Salir"  
-        };
-        int seleccion = JOptionPane.showOptionDialog(null, "Seleccione una opción", "Menú principal", 0,0, icon,options,options[0]);
-        return seleccion;
-    }
-    
-    public static String menu_insercion(){
-        icon = new MyIcon();
-        String seleccion = JOptionPane.showInputDialog(null, "Escriba una cadena", "Escribir cadena", -1);
-        return seleccion;
-    }
-    
-    public static void menu_salida(){
-        icon = new MyIcon();
-        JOptionPane.showMessageDialog(null, "Hasta luego", "Salir", 0, icon);
+        Ventana v1 = new Ventana();
+        v1.setVisible(true);
     }
     
     public static void error_message(){
---
+++ src/analizador_lexico/Ventana.java
@@ -0,0 +1,11 @@
+package analizador_lexico;
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame{
+    
+    public Ventana(){
+        this.setSize(600, 600);
+        
+    }
+    
+}
